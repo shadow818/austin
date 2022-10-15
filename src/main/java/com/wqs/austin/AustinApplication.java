107d974a6fde9aa6e57c03843171f90ac9f4fa7e
wqs <1103052867@qq.com>
2022-10-15 15:55:35 +0800

    init

---
+++ src/main/java/com/wqs/austin/AustinApplication.java
@@ -0,0 +1,13 @@
+package com.wqs.austin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AustinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AustinApplication.class, args);
+    }
+
+}
